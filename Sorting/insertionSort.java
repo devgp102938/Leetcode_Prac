6be27bcc986f9f203299ceff907880d7afdd90f0
Dev Patel <devp78101@gmail.com>
2026-07-11 16:42:24 +0530

    merge and insertion sort

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 class insertionSort{
     public int[] insertionsort(int arr[], int n){
         for(int i = 1; i < n; i++){
-            int curr = arr[i];
-            int prev = i - 1;
+            int key = arr[i];
+            int j = i - 1;
 
-            while(prev >= 0 && arr[prev] > curr){
-                arr[prev + 1] = arr[prev];
-                prev--;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j--;
             }
 
-            arr[prev + 1] = curr;
+            arr[j + 1] = key;
         }
         return arr;
     }
