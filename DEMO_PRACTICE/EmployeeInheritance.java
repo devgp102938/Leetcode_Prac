2c0229fbd6a1d6d1ab94eb19c3994f5189722ded
Dev Patel <devp78101@gmail.com>
2026-08-22 18:32:41 +0530

    coding

--- DEMO_PRACTICE/EmployeeInheritance.java
+++ DEMO_PRACTICE/EmployeeInheritance.java
@@ -36,7 +36,7 @@ class EmployeeInheritance{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter name ");
+        System.out.println("Enter name: ");
         String name = sc.nextLine();
 
         System.out.println("Enter Salary: ");
